25473d97d4710d53914f14912bb0003fbfa82fca
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-06-20 16:40:17 +0300

    Add files via upload

---
+++ precache-manifest.760f6caa854c02097269e4a5306c8e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e8d565f8b447f3027a315a1cfba941",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "74891b76d6f43098b59a",
+    "url": "/test-task/static/css/main.8a833a90.chunk.css"
+  },
+  {
+    "revision": "6090c83908ee6745c657",
+    "url": "/test-task/static/js/2.6475de71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task/static/js/2.6475de71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74891b76d6f43098b59a",
+    "url": "/test-task/static/js/main.a0a2deaf.chunk.js"
+  },
+  {
+    "revision": "c019f8f22c150978fcb3",
+    "url": "/test-task/static/js/runtime-main.02a0f182.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
